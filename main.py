31e4a0d4e44f2d17d85d6c2d285690d66e66fa1d
Michael <69639434+intenebris@users.noreply.github.com>
2026-08-31 11:05:36 +0000

    Добавлена генерация случайных символов

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 DIGITS = "0123456789"
 SPECIAL = "!@#$%^&*"
 
-def generate_password(length: int, use_uppercase=True, use_digits=True, use_special=False) -> str:
+def next_random(number):
+    return (16807 * number) % 2147483647
+
+def generate_password(length: int, seed: int, use_uppercase=True, use_digits=True, use_special=False) -> str:
     alphabet = LOWERCASE
     if use_uppercase:
         alphabet += UPPERCASE
@@ -11,16 +14,19 @@ def generate_password(length: int, use_uppercase=True, use_digits=True, use_spec
         alphabet += DIGITS
     if use_special:
         alphabet += SPECIAL
+    current = seed
     result = ""
-    for i in range(0, length - 1):
-        index = i % len(alphabet)
+    i = 0
+    while i < length:
+        current = next_random(current)
+        index = current % len(alphabet)
         result += alphabet[index]
+        i += 1
     return result
 
-print(generate_password(5))  # => "abcde"
-print(generate_password(30))  # => "abcdefghijklmnopqrstuvwxyzabcd"
-print(generate_password(0))  # => ""
-print(generate_password(length=30, use_uppercase=False, use_digits=False))
-# набор = строчные + ПРОПИСНЫЕ + цифры + спецсимволы (70 символов)
-print(generate_password(length=70, use_special=True))
-# => "...0123456789!@#$%^&*"
+print(generate_password(length=8, seed=1))
+print(generate_password(length=12, seed=123))
+print(generate_password(length=12, seed=123, use_special=True))
+print(generate_password(length=8, seed=1, use_uppercase=False, use_digits=False))
+print(generate_password(length=-3, seed=42))
+print(generate_password(length=18, seed=4, use_special=True))
